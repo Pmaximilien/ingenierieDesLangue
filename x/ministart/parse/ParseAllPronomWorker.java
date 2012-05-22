b189c04461a78a14a35b068f415472347e2773cd
rpesche <romain.pesche@gmail.com>
2012-05-22 15:17:52 +0200

    Ajout détection des phrases comme «Je mange une pomme» et «Nous nous retrouvons dans une cave»

--- x/ministart/parse/ParseAllPronomWorker.java
+++ x/ministart/parse/ParseAllPronomWorker.java
@@ -19,9 +19,46 @@ import x.ministart.utils.Chrono;
  * @author Douglas DUTEIL
  *
  */
+//private String Get_Mot(Document doc, int ind){
+
+
 public class ParseAllPronomWorker{
 
     private SwingUI ui;
+
+
+
+    private int skip_sujet(Document doc, int ind){
+
+
+    	String verbe = "<gv>";
+    	String fin_verbe = "</gv>";
+	String next = Get_Mot(doc, ind);
+	System.out.println("Premier mot : "+next);
+	System.out.println("Deuxieme mot : "+ Get_Mot(doc, ind + next.length()));
+	ind += next.length();
+
+	// Je | mange un fruit
+	if (isPronom(next)){
+		next = Get_Mot(doc, ind);
+
+		// Je | me fait des patte
+		if (isSecondPronom(next))
+			ind += next.length();
+			// Je me | fait des patte
+	} else {
+		return 0;
+	}
+
+
+
+
+	return ind;
+
+
+
+
+	}
 
     public ParseAllPronomWorker(final SwingUI ui) {
 	this.ui = ui;
@@ -62,9 +99,13 @@ public class ParseAllPronomWorker{
 
 		String debut = "<phrase>";
 		String fin = "</phrase>";
+		String verbe = "</gv>";
 
 		int sentence_size;
 		int mine = 0;
+		int gv;
+
+
 		for (int txt_ind = 0; txt_ind < docLen; txt_ind++) {
 			doc.insertString(txt_ind, debut, null);
 			txt_ind += 8;
@@ -72,6 +113,13 @@ public class ParseAllPronomWorker{
 			while(! isDelimiteur( doc.getText(txt_ind + sentence_size, 1))){
 				sentence_size++;
 			}
+
+    			gv = skip_sujet(doc, txt_ind);
+			if (gv != 0){
+				doc.insertString(gv, verbe, null);
+				txt_ind += verbe.length();
+			}
+	
 			txt_ind += sentence_size;
 			doc.insertString(txt_ind, fin, null);
 			txt_ind += 9;
@@ -121,11 +169,25 @@ public class ParseAllPronomWorker{
 	int taille = 0;
 
 	int max = doc.getLength();
+
+	try {
+	mot = doc.getText(ind++, 1);
+	 } catch (BadLocationException e) {
+		e.printStackTrace();
+	throw new RuntimeException(e);
+	   }
+	   
+
+	if (mot.equals("\n"))
+		ret.append(" ");
+	else
+		ret.append(mot);
+
 	while(true){
 	    try 
 	    {
 		mot = doc.getText(ind++, 1);
-		if (mot.equals(" ") || mot.equals("'") || ind >= max) 
+		if (mot.equals(" ")) // fin du mot
 		    break;
 		ret.append(mot);
 
@@ -138,12 +200,41 @@ public class ParseAllPronomWorker{
 	return ret.toString();
     }
 
-    private boolean isDelimiteur(String mot){
-	String [] pronoms = {".", "!", "?"};
+    private boolean isPronom(String mot){
+	String [] pronoms = {"je", "tu", "il", "elle","ont", "nous", "vous", "ils", "elles"};
 	int i;
+	mot = mot.trim();
 
-	for(i = 0; i < 3; i++){
+	for(i = 0; i < pronoms.length; i++){
 	    if(mot.equalsIgnoreCase(pronoms[i]))
+		return true;
+	}
+	return false;
+    }
+
+
+     private boolean isSecondPronom(String mot){
+	String [] pronoms = {"me", "te", "le", "la", "nous", "vous", "les", "elles"};
+
+	int i;
+	 
+	mot = mot.trim();
+
+	for(i = 0; i < pronoms.length; i++){
+	    if(mot.equalsIgnoreCase(pronoms[i]))
+		return true;
+	}
+	return false;
+    }
+    	
+
+    private boolean isDelimiteur(String mot){
+	String [] stop = {".", "!", "?", ","};
+	int i;
+	mot = mot.trim();
+
+	for(i = 0; i < stop.length; i++){
+	    if(mot.equalsIgnoreCase(stop[i]))
 		return true;
 	}
 	return false;
